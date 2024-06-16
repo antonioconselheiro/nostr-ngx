b1e753533c6245a09b525249ce8381acdfae79fb
António Conselheiro <anarcanudos@proton.me>
2024-06-16 14:14:21 -0300

    revisão do file manager

--- projects/nostr-credential-ngx/src/lib/shared/file-manager/file-manager.module.ts
+++ projects/nostr-credential-ngx/src/lib/shared/file-manager/file-manager.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FileManagerService } from './file-manager-service';
+import { FileManagerService } from './file-manager.service';
 
 @NgModule({
   imports: [
