1c3fb560c0973a1393d5f38957a096990a8312bc
António Conselheiro <anarcanudos@proton.me>
2024-06-25 08:41:11 -0300

    template do formulário inicial para inclusão de relays

--- projects/nostr-credential-ngx/src/lib/credential-manager-widget/signup/relay-manager/my-relays/my-relays.component.ts
+++ projects/nostr-credential-ngx/src/lib/credential-manager-widget/signup/relay-manager/my-relays/my-relays.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { NostrSigner } from '@belomonte/nostr-credential-ngx';
+import { AuthModalSteps } from '../../../auth-modal-steps.type';
 
 @Component({
   selector: 'nostr-my-relays',
@@ -7,4 +10,25 @@ import { Component } from '@angular/core';
 })
 export class MyRelaysComponent {
 
+  @Output()
+  changeStep = new EventEmitter<AuthModalSteps>();
+
+  myRelaysForm!: FormGroup<{
+    relaysFrom: FormControl<string | null>;
+  }>;
+
+  constructor(
+    private fb: FormBuilder,
+    private nostrSigner: NostrSigner
+  ) { }
+
+  ngOnInit(): void {
+    this.initForm();
+  }
+
+  private initForm(): void {
+    this.myRelaysForm = this.fb.group({
+      relaysFrom: ['']
+    });
+  }
 }
--- projects/nostr-credential-ngx/src/lib/credential-manager-widget/signup/relay-manager/relay-manager.module.ts
+++ projects/nostr-credential-ngx/src/lib/credential-manager-widget/signup/relay-manager/relay-manager.module.ts
@@ -4,6 +4,8 @@ import { RelayManagerComponent } from './relay-manager.component';
 import { RelaySearchComponent } from './relay-search/relay-search.component';
 import { RelayDetailComponent } from './relay-detail/relay-detail.component';
 import { MyRelaysComponent } from './my-relays/my-relays.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { SvgRenderModule } from '../../../svg-render/svg-render.module';
 
 @NgModule({
   declarations: [
@@ -13,7 +15,9 @@ import { MyRelaysComponent } from './my-relays/my-relays.component';
     MyRelaysComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    SvgRenderModule,
+    ReactiveFormsModule
   ],
   exports: [
     RelayManagerComponent
