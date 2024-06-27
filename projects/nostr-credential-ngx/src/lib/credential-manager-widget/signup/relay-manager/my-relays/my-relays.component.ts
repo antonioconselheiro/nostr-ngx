8a2d20db9976b8f570d44abc91dd967ce155c818
António Conselheiro <anarcanudos@proton.me>
2024-06-27 19:43:19 -0300

    criando componente de adição de relay

--- projects/nostr-credential-ngx/src/lib/credential-manager-widget/signup/relay-manager/my-relays/my-relays.component.ts
+++ projects/nostr-credential-ngx/src/lib/credential-manager-widget/signup/relay-manager/my-relays/my-relays.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { TAuthModalSteps } from '../../../auth-modal-steps.type';
 import { NostrSigner } from '../../../../profile-service/nostr.signer';
 import { TRelayManagerSteps } from '../relay-manager-steps.type';
+import { TRelayMap } from '@belomonte/nostr-ngx';
 
 @Component({
   selector: 'nostr-my-relays',
@@ -17,9 +18,12 @@ export class MyRelaysComponent {
   @Output()
   changeRelayStep = new EventEmitter<TRelayManagerSteps>();
 
+  choosingRelays: TRelayMap = {};
+
   myRelaysForm!: FormGroup<{
     relaysFrom: FormControl<string | null>;
     newRelay: FormControl<string | null>;
+    newCacheRelay: FormControl<string | null>;
   }>;
 
   constructor(
@@ -34,7 +38,36 @@ export class MyRelaysComponent {
   private initForm(): void {
     this.myRelaysForm = this.fb.group({
       relaysFrom: ['public'],
-      newRelay: ['']
+      newRelay: [''],
+      newCacheRelay: ['']
     });
+  }
+
+  listRelays(): Array<{
+    relay: string;
+    read: boolean;
+    write: boolean;
+  }> {
+    return Object
+      .keys(this.choosingRelays)
+      .map(relay => ({ ...this.choosingRelays[relay], relay }));
+  }
+
+  removeRelay(relay: string): void {
+    delete this.choosingRelays[relay];
+  }
+
+  connect(relay: string): void {
+    this.choosingRelays[relay] = {
+      read: true,
+      write: true
+    };
+  }
+
+  connectCache(relay: string) {
+    this.choosingRelays[relay] = {
+      read: true,
+      write: false
+    };
   }
 }
--- projects/nostr-credential-ngx/src/lib/credential-manager-widget/signup/relay-manager/relay-manager.component.ts
+++ projects/nostr-credential-ngx/src/lib/credential-manager-widget/signup/relay-manager/relay-manager.component.ts
@@ -11,4 +11,6 @@ export class RelayManagerComponent {
   changeStep = new EventEmitter<TAuthModalSteps>();
 
   currentStep: TRelayManagerSteps = 'myRelays';
+
+  relayDetail?: string;
 }
