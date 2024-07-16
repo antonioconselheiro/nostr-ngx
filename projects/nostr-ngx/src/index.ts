624ba5011bdb992f07ea1cd8b23d917d21a4bcdb
António Conselheiro <anarcanudos@proton.me>
2024-07-16 14:16:45 -0300

    removendo interfaces presentes na nova atualização do nostr-tools

--- projects/nostr-ngx/src/index.ts
+++ projects/nostr-ngx/src/index.ts
@@ -1,6 +1,6 @@
-import { EventTemplate, NostrEvent } from 'nostr-tools';
+import { WindowNostr } from 'nostr-tools/nip07';
 
-export * from './domain/amount-type.type'
+export * from './domain/amount-type.type';
 export * from './domain/ncryptsec.type';
 export * from './domain/nip05.type';
 export * from './domain/nostr-event-kind.enum';
@@ -8,39 +8,25 @@ export * from './domain/nostr-local-config.interface';
 export * from './domain/nostr-public.type';
 export * from './domain/nostr-secret.type';
 export * from './domain/nostr-session-config.interface';
-export * from './domain/relay-record.type';
 export * from './domain/relay-config.interface';
 export * from './domain/relay-information.interface';
+export * from './domain/relay-record.type';
 
 export * from './shared/abstract-storage';
-export * from './shared/nostr/no-credentials-found.error';
 export * from './shared/nostr/configs-local.storage';
 export * from './shared/nostr/configs-session.storage';
+export * from './shared/nostr/derivated.pool';
+export * from './shared/nostr/extended.pool';
+export * from './shared/nostr/no-credentials-found.error';
+export * from './shared/nostr/nostr.converter';
 export * from './shared/nostr/nostr.guard';
 export * from './shared/nostr/nostr.module';
 export * from './shared/nostr/nostr.service';
-export * from './shared/nostr/nostr.converter';
-export * from './shared/nostr/signer-not-found.error';
-export * from './shared/nostr/pool.statefull';
-export * from './shared/nostr/extended.pool';
-export * from './shared/nostr/derivated.pool';
-export * from './shared/nostr/smart.pool';
-export * from './shared/nostr/observe-relay.fn';
 export * from './shared/nostr/observe-pool.fn';
-
-export interface WindowNostr {
-  getPublicKey(): Promise<string>
-  signEvent(event: EventTemplate): Promise<NostrEvent>
-  getRelays(): Promise<any>
-  nip04?: {
-    encrypt(pubkey: string, plaintext: string): Promise<string>
-    decrypt(pubkey: string, ciphertext: string): Promise<string>
-  }
-  nip44?: {
-    encrypt(pubkey: string, plaintext: string): Promise<string>
-    decrypt(pubkey: string, ciphertext: string): Promise<string>
-  }
-}
+export * from './shared/nostr/observe-relay.fn';
+export * from './shared/nostr/pool.statefull';
+export * from './shared/nostr/signer-not-found.error';
+export * from './shared/nostr/smart.pool';
 
 declare global {
   interface Window {
