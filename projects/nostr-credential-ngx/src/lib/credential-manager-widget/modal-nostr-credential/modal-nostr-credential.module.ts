65e8a740367d7cf86b1d153110496e4164da5ad7
António Conselheiro <anarcanudos@proton.me>
2024-07-29 10:35:45 -0300

    iniciando tela de pesquisa de relays

--- projects/nostr-credential-ngx/src/lib/credential-manager-widget/modal-nostr-credential/modal-nostr-credential.module.ts
+++ projects/nostr-credential-ngx/src/lib/credential-manager-widget/modal-nostr-credential/modal-nostr-credential.module.ts
@@ -1,17 +1,16 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ModalNostrCredentialComponent } from './modal-nostr-credential.component';
-import { SelectAccountModule } from '../select-account/select-account.module';
-import { AuthenticateFormModule } from '../authenticate-form/authenticate-form.module';
-import { LoginFormModule } from '../login-form/login-form.module';
+import { NgModule } from '@angular/core';
 import { LoadingWidgetModule } from '../../loading/loading-widget.module';
-import { RegisterAccountFormModule } from '../signup/register-account/register-account.module';
-import { CreateNsecFormModule } from '../signup/create-nsec/create-nsec.module';
-import { RelayManagerModule } from '../signup/relay-manager/relay-manager.module';
-import { ChoosePasswordModule } from '../signup/choose-password/choose-password.module';
+import { AuthenticateFormModule } from '../authenticate-form/authenticate-form.module';
 import { DownloadSignerModule } from '../download-signer/download-signer.module';
+import { LoginFormModule } from '../login-form/login-form.module';
+import { SelectAccountModule } from '../select-account/select-account.module';
+import { ChoosePasswordModule } from '../signup/choose-password/choose-password.module';
 import { CreateNsecAndNcryptsecModule } from '../signup/create-nsec-and-ncryptsec/create-nsec-and-ncryptsec.module';
-import { SearchRelaysModule } from '../signup/search-relays/search-relays.module';
+import { CreateNsecFormModule } from '../signup/create-nsec/create-nsec.module';
+import { RegisterAccountFormModule } from '../signup/register-account/register-account.module';
+import { RelayManagerModule } from '../signup/relay-manager/relay-manager.module';
+import { ModalNostrCredentialComponent } from './modal-nostr-credential.component';
 
 @NgModule({
   declarations: [
@@ -28,8 +27,7 @@ import { SearchRelaysModule } from '../signup/search-relays/search-relays.module
     CreateNsecAndNcryptsecModule,
     ChoosePasswordModule,
     RelayManagerModule,
-    DownloadSignerModule,
-    SearchRelaysModule
+    DownloadSignerModule
   ],
   exports: [
     ModalNostrCredentialComponent
