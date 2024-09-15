9aac9c5d95b3208cddd76c8558b9c76eaafde5ed
António Conselheiro <anarcanudos@proton.me>
2024-09-15 19:20:01 -0300

    rename nostr-credential-ngx to nostr-ui-ngx

--- build.sh
+++ build.sh
@@ -2,15 +2,15 @@
 
 rm -rf dist;
 npm run build:nostr;
-npm run build:credential;
+npm run build:gui;
 
 cd ./dist/nostr-ngx;
 npm pack;
 
-cd ../nostr-credential-ngx;
+cd ../nostr-gui-ngx;
 npm pack;
 
 cd ../..;
 chmod 777 -R dist;
-mv ./dist/nostr-credential-ngx/belomonte-nostr-credential-ngx-0.0.1.tgz ./dist/belomonte-nostr-credential-ngx-0.0.1.tgz
+mv ./dist/nostr-gui-ngx/belomonte-nostr-gui-ngx-0.0.1.tgz ./dist/belomonte-nostr-gui-ngx-0.0.1.tgz
 mv ./dist/nostr-ngx/belomonte-nostr-ngx-0.0.1.tgz ./dist/belomonte-nostr-ngx-0.0.1.tgz
