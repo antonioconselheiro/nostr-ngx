42219ec446e2fc8c89eff9aef4046c70cf2e4d9c
António Conselheiro <anarcanudos@proton.me>
2024-10-08 21:29:54 -0300

    atualizando método de inscrição em filtro

--- projects/nostr-ngx/src/lib/pool/nostr.pool.ts
+++ projects/nostr-ngx/src/lib/pool/nostr.pool.ts
@@ -1,6 +1,6 @@
 import { Inject, Injectable } from '@angular/core';
 import { NCache, NostrEvent, NostrFilter, NostrRelayCLOSED, NostrRelayEOSE, NostrRelayEVENT } from '@nostrify/nostrify';
-import { filter, from, map, Observable, of, takeUntil } from 'rxjs';
+import { filter, finalize, from, map, Observable, takeUntil } from 'rxjs';
 import { MAIN_NCACHE_TOKEN } from '../injection-token/main-ncache.token';
 import { FacadeNPool } from './facade.npool';
 import { NPoolRequestOptions } from './npool-request.options';
@@ -25,23 +25,45 @@ export class NostrPool extends FacadeNPool {
   }
 
   //  TODO: verificar se a incrição e desinscrição estão funcionando corretamente
-  observe(filters: Array<NostrFilter>, opts?: NPoolRequestOptions): Observable<NostrEvent> {
-    const observable = from(this.req(filters, opts));
-    const closedSignal$ = observable.pipe(
-      filter(([kind]) => kind === 'CLOSED'),
-      takeUntil(of(undefined)) 
+  observe(filters: Array<NostrFilter>): Observable<NostrEvent> {
+    console.info('[[subscribe filter]]', filters);
+    const abort = new AbortController();
+    const observable = from(this.req(filters, abort));
+    const relayClosed$ = observable.pipe(
+      filter(([kind]) => kind === 'CLOSED')
     );
 
-    const abort = new AbortController();
-    opts = opts || {};
+    observable.subscribe({
+      next: event => {
+        console.debug('[DEBUG] something found by filters:', filters, 'event:', event);
+      },
+      error: error => {
+        console.debug('[DEBUG] ERROR received from filters:', filters, 'error:', error);
+      },
+      complete: () => {
+        console.debug('[DEBUG] Filter completed:', filters);
+      }
+    });
 
-    closedSignal$.subscribe(() => abort.abort());
+    relayClosed$.subscribe(() => {
+      console.info('[[unsubscribe filter]]', filters);
+      abort.abort();
+    });
   
     return observable
       .pipe(
+        finalize(() => {
+          console.info('[[unsubscribe filter]]', filters);
+          abort.abort();
+        })
+      )
+      .pipe(
         filter(([kind]) => kind === 'EVENT'),
-        takeUntil(closedSignal$)
-      ).pipe(map(([,,event]) => event as NostrEvent));
+        takeUntil(relayClosed$),
+      ).pipe(map(([,,event]) => {
+        console.info('[[filter found event]]', event, filters)
+        return event as NostrEvent;
+      }));
   }
 
   override req(
