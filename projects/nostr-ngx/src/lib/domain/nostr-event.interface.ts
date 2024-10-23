63c7268fa51e62f6537fcf3d8a1b7ffe1adbaa93
António Conselheiro <anarcanudos@proton.me>
2024-10-23 15:17:50 -0300

    try harder

--- projects/nostr-ngx/src/lib/domain/nostr-event.interface.ts
+++ projects/nostr-ngx/src/lib/domain/nostr-event.interface.ts
@@ -1,12 +1,26 @@
 import { verifiedSymbol } from 'nostr-tools'
 
+//  why this class exists?
+//  https://github.com/nbd-wtf/nostr-tools/pull/448
+//  https://github.com/soapbox-pub/nostrify/issues/5
+/**
+ * Nostr Event
+ */
 export interface NostrEvent<T extends number = number> {
-  kind: T
-  tags: string[][]
-  content: string
-  created_at: number
-  pubkey: string
-  id: string
-  sig: string
+  /** 32-bytes lowercase hex-encoded sha256 of the serialized event data. */
+  id: string;
+  /** 32-bytes lowercase hex-encoded public key of the event creator */
+  pubkey: string;
+  /** Unix timestamp in seconds. */
+  created_at: number;
+  /** Integer between 0 and 65535. */
+  kind: T;
+  /** Matrix of arbitrary strings. */
+  tags: string[][];
+  /** Arbitrary string. */
+  content: string;
+  /** 64-bytes lowercase hex of the signature of the sha256 hash of the serialized event data, which is the same as the `id` field. */
+  sig: string;
+
   [verifiedSymbol]?: boolean
 }
