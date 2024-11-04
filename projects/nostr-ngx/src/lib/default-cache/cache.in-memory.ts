09f74d0850ec482651a26caca88503e7bfa99fbd
António Conselheiro <anarcanudos@proton.me>
2024-11-04 11:13:59 -0300

    review container auth approch

--- projects/nostr-ngx/src/lib/default-cache/cache.in-memory.ts
+++ projects/nostr-ngx/src/lib/default-cache/cache.in-memory.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
 import { InMemoryNCache } from '../injection-token/in-memory.ncache';
+import { NostrEvent } from '../domain/event/nostr-event.interface';
+import { HexString } from '../domain/event/primitive/hex-string.type';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CacheInMemory extends InMemoryNCache {
-
+  override get(key: HexString): NostrEvent {
+    
+  }
 }
