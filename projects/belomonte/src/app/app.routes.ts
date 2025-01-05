4e3ee91a268e1a55bff2a27d53c12dfd006258ab
António Conselheiro <anarcanudos@proton.me>
2025-01-05 10:16:51 -0300

    removendo estrutura do projeto de validação da biblioteca, a validação será feita no twitter parody

--- projects/belomonte/src/app/app.routes.ts
+++ projects/belomonte/src/app/app.routes.ts
@@ -1,15 +1,3 @@
 import { Routes } from '@angular/router';
-import { PoolInteractComponent } from './pages/pool-interact/pool-interact.component';
-import { NostrAuthCheckComponent } from './pages/nostr-auth-check/nostr-auth-check.component';
 
-export const routes: Routes = [
-  {
-    path: 'pool-interact',
-    component: PoolInteractComponent
-  },
-
-  {
-    path: 'nostr-auth',
-    component: NostrAuthCheckComponent
-  }
-];
+export const routes: Routes = [ ];
